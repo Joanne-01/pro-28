9986797d9db8b25b45a233440413f3515313df04
Joanne-01 <75727253+Joanne-01@users.noreply.github.com>
2021-03-11 14:03:50 +1100

    Add files via upload

---
+++ stone.js
@@ -0,0 +1,15 @@
+class stone{
+constructor(x,y){
+   var options={
+       isStatic:false,
+       restitution:0,
+       friction:1,
+       density:1.2
+   }
+   this.x=x;
+   this.y=y; 
+   this.image=loadImage("images/stone.png")
+   this.body=Bodies.circle(this.x, this.y, options)
+   World.add(world, this.body);
+}
+}
